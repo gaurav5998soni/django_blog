5333dabd3bc8de9ba76f9ca4717129f67b201b68
gaurav5998soni <41364696+gaurav5998soni@users.noreply.github.com>
2019-01-17 19:44:41 +0530

    Update settings.py

--- mydjango_project/mydjango_project/settings.py
+++ mydjango_project/mydjango_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!j3n!f%&_!)h!f$+mdk@hf7%q_-#hapjj$l!yxnd^^efdb2dgb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
